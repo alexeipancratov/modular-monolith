69b2ad0586d1dda3fc68b0170a7d7c3a077e153c
Alexei Pancratov <drivergear@gmail.com>
2024-03-23 20:39:52 -0400

    Added a few books

--- src/RiverBooks.Books/BookService.cs
+++ src/RiverBooks.Books/BookService.cs
@@ -8,7 +8,9 @@ internal class BookService : IBookService
         [
             new BookDto(Guid.NewGuid(), "The Fellowship of the Ring", "J.R.R. Tolkien"),
             new BookDto(Guid.NewGuid(), "The Two Towers", "J.R.R. Tolkien"),
-            new BookDto(Guid.NewGuid(), "The Return of the King", "J.R.R. Tolkien")
+            new BookDto(Guid.NewGuid(), "The Return of the King", "J.R.R. Tolkien"),
+            new BookDto(Guid.NewGuid(), "The Hobbit", "J.R.R. Tolkien"),
+            new BookDto(Guid.NewGuid(), "Master and Margarita", "Mikhail Bulgakov")
         ];
     }
 }
