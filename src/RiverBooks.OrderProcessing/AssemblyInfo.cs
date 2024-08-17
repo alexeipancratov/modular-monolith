47b6ea0d7fb4b5a78f9a5ff23aa33c5d0fa96388
Alexei Pancratov <drivergear@gmail.com>
2024-08-17 15:58:58 -0400

    Added ArchUnit test for OrderProcessing module

---
+++ src/RiverBooks.OrderProcessing/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("RiverBooks.OrderProcessing.Tests")]
+namespace RiverBooks.OrderProcessing;
+
+internal class AssemblyInfo
+{ }
